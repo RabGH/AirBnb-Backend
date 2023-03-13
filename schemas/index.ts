99a3b6fc3eb1a88be23363662608213a547704fa
RabGh <rabiighais@gmail.com>
2023-03-13 12:32:05 +0200

    Backend Sanity Initial Commit

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+import host from './host'
+import person from './person'
+import property from './property'
+import propertyImage from './propertyImage'
+import review from './review'
+import traveller from './traveller'
+
+export const schemaTypes = [
+    property, 
+    propertyImage, 
+    review,
+    traveller,
+    host,
+    person
+]
---
+++ schemas/propertyImage.ts
@@ -0,0 +1,18 @@
+export default {
+    name: 'propertyImage',
+    title: 'Property Image',
+    type: 'image',
+    fields: [
+        {
+            name: 'caption',
+            title: 'Caption',
+            type: 'string',
+            options: {
+                inHightLight: true
+            }
+        },
+    ],
+    options: {
+        hotspot: true,
+    }
+}
